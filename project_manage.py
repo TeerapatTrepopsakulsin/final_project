add2466f4c90d2c0c2c0a26c4713d7168c8a373d
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2023-11-23 10:11:31 +0700

    draft for lead role

--- project_manage.py
+++ project_manage.py
@@ -72,17 +72,44 @@ def student():
     print('Select your action')
     print('1. Requests')
     print('2. Create a project')
-    choice = int(input('Input number(1-2): '))
-    if choice == 1:
-        print(member_pending_request.table)
-        print('Accept or Deny')
-    if choice == 2:
-        print(project.table)
+    print('3. Exit')
+    choice = int(input('Input number(1-3): '))
+    while choice != 3:
+        if choice == 1:
+            print(member_pending_request.table)
+            print('Accept or Deny')
+        elif choice == 2:
+            print(project.table)
 
 
+def lead():
+    print('Select your action')
+    print('1. See project status')
+    print('2. Modify project information')
+    print('3. Responded requests')
+    print('4. Send out members requests')
+    print('5. Send out advisor requests')
+    print('6. Exit')
+    choice = int(input('Input number(1-6): '))
+    while choice != 6:
+        if choice == 1:
+            print(project.table)
+        elif choice == 2:
+            print(project.table)
+            print('modify or not')
+        elif choice == 3:
+            print(member_pending_request.table)
+        elif choice == 4:
+            print(member_pending_request.table)
+            print('send request 1 at a time')
+            member_pending_request.update('member request')
+        elif choice == 5:
+            print(advisor_pending_request.table)
+            print('send request 1 at a time')
+            advisor_pending_request.update('advisor request')
 
 
-# make calls to the initializing and login functions defined above
+        # make calls to the initializing and login functions defined above
 
 initializing()
 val = login()
